f3c48ea52bbaa0f861727bd50606c5c82c6ba3f3
MuradAle <muradik1999@outlook.com>
2021-10-21 18:18:08 -0400

    New_Navbar
    you can see some difference

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: 'http://localhost:3000/Home',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Never gonna give u up',
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact Us',
+        url: 'ContactUs',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Sign up',
+        url: 'http://localhost:3000/SignUp',
+        cName: 'nav-links-mobile',
+    },
+]
