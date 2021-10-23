f8e393d58671189646cfe45ba2b577280c90bd2f
mitchellmui17 <mitchellmui17@gmail.com>
2021-10-22 22:09:12 -0400

    Adding backend
    +auth
    +signup page

--- src/App.js
+++ src/App.js
@@ -5,30 +5,33 @@ import Navbar from './components/NavBar/NavBar';
 import Calendar from './components/Calendar/Calendar';
 import React from "react"
 import { Container, NavItem } from 'react-bootstrap';
+import { AuthProvider } from './context/AuthContext.js';
 
 import { BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <main>
-        <Switch>   
-          <Route path="/Home" exact> 
-            <LandingPage />
-          </Route>
-          <Route path="/Calendar" exact> 
-            <Calendar />
-          </Route>
-          <Route path="/ContactUs" exact>
-            <ContactUs />
-          </Route>
-          <Route path="/SignUp" exact>
-            <SignUp />
-          </Route>
-        </Switch>
-      </main>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Switch>   
+            <Route path="/Home" exact> 
+              <LandingPage />
+            </Route>
+            <Route path="/Calendar" exact> 
+              <Calendar />
+            </Route>
+            <Route path="/ContactUs" exact>
+              <ContactUs />
+            </Route>
+            <Route path="/SignUp" exact>
+              <SignUp />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </AuthProvider>
   );
 }
 export default App;
