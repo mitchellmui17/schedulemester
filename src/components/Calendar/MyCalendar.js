443c31695048ad6839ad19f4e111cfda7e9f6ada
MuradAle <muradik1999@outlook.com>
2021-10-31 19:01:59 -0400

    fixed navbar/calendar
    Navbar and calendar been fixed

--- src/components/Calendar/MyCalendar.js
+++ src/components/Calendar/MyCalendar.js
@@ -26,18 +26,21 @@ const events = [
         allDay: true,
         start: new Date(2021, 9, 29),
         end: new Date(2021 ,9, 29),
+        cName:"Event",
     },
     {
         title: "1st-iteration",
         allDay: true,
         start: new Date(2021, 10, 3),
         end: new Date(2021 ,10, 3),
+        cName:"Event",
     },
     {
         title: "2st-iteration",
         allDay: true,
         start: new Date(2021, 10, 17),
         end: new Date(2021 ,10, 17),
+        cName:"Event",
     },
 ];
 
@@ -50,19 +53,24 @@ function MyCalendar() {
     }
 
     return (
-        <div className="App">
-            <h1>Calendar</h1>
-            <h2>Add New Event</h2>
-            <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
-                    Add Event
-                </button>
+            <div className="App">
+                <div className="Container">
+                    <div className="Cal_Header">
+                        <h1 className="1_header">Calendar</h1>
+                    </div>
+                    <div className = "info">
+                        <h2 className="2_header">Add New Event</h2>
+                        <div className="menu">
+                            <input className="DataPicker_1" type="text" placeholder="Add Title" value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />                                <DatePicker className="DataPicker_1" placeholderText="Start Date" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                            <DatePicker className="DataPicker_1" placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                            <button className="Button" onClick={handleAddEvent}>
+                                Add Event
+                            </button>
+                        </div>
+                    </div>
+                    <Calendar className="Calen" localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+                </div>
             </div>
-            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
-        </div>
     );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
   render () {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+        <h1 className="navbar-logo">Schedulemester<i className="fab fa-react"></i></h1>
         <div className="menu-icon" onClick={this.handleClick}>
           <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
@@ -28,7 +28,6 @@ class Navbar extends Component {
             )
           })}
         </ul>
-        <Button>Sign Up</Button>
       </nav>
     )
   }
