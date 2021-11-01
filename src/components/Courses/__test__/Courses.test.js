c1c2c629544f4e01877adf4dd4fcb4223e399d2e
DD155 <31744748+DD155@users.noreply.github.com>
2021-11-01 15:50:32 -0400

    added initial unit tests for rendering for other components

---
+++ src/components/Courses/__test__/Courses.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Courses from './../Courses';
+
+it('renders without crashing', () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Courses></Courses>, div);
+});
