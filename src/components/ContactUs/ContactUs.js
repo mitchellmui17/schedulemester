cfbba6c304bb12eed007857b988e0bc9eb63521d
MuradAle <muradik1999@outlook.com>
2021-11-03 15:34:24 -0400

    Home/AboutUs
    small description of Homepage and AboutUs page

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,24 +1,29 @@
 import React, {Component} from "react";
-
+import about from "../../images/about.jpg"
 
 
 class ContactUs extends Component{
-
-    
-    render(){
-
-        return(
-            <div>
-            <div className ="welcome-template">
-                <div className = "welcome-comment">
-                    <span className ="welcome">ContactUs</span><br></br>
+        render() {
+            return (
+                <div className = "back">
+                    <div class="left-half">
+                        <img className="image_time" src={about} alt="about"/>
+                    </div>
+                    <div class="right-half">
+                        <article>
+                            <h1>About Us</h1>
+                            <p>Tracker is keeping track of everyone progress status, and the documenting how the way how they measure and track.</p>
+                            <p>Unit Tester is writing unit testing for the application and tutorial on how to use the unit testing.</p>
+                            <p>Code Reviewer is responsible for the request pull from the github and making sure the review process and coding standard 
+                            for the projects.</p>
+                            <p>Continues Integrator is to making sure every pull request is working successful and establishing the continuous integration.</p>
+                            <p>Customer is to making sure the user stories will be created  that define the prioritizing for each release</p>
+                            <p>And coming up with acceptance criteria for each of the user stories.</p>
+                            <p>Designed is response for using Figma to keep track of the UI of the website and how we want to designed it.</p>
+                        </article>
+                    </div>
                 </div>
-
-            </div>
-            {/* <Footer/> */}
-            </div>
-            
-        )
+            );
+        }
     }
-}
 export default ContactUs;
