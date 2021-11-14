12ba517c29dda1581a06465147c352328e93647a
mitchellmui17 <mitchellmui17@gmail.com>
2021-11-14 15:45:47 -0500

    Fixed api key string

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import "firebase/auth";
 
 
 var app = firebase.initializeApp({
-  apiKey: "AIzaSyCgzHacvD_01nxj2sdMSdODtsNSh5pi65U",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
