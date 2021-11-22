2ad47df0071a922792a560b9eea72a065cd3976f
MuradAle <65867559+MuradAle@users.noreply.github.com>
2021-11-22 16:29:28 -0500

    test
    test

--- src/components/Calendar/AddEvents.js
+++ src/components/Calendar/AddEvents.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import eachDayOfInterval from "date-fns/eachDayOfInterval";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -13,75 +13,126 @@ export default function AddEvents() {
     const [ allStart, setAllStart ] = useState([])
     const [ allEnd, setAllEnd ] = useState([])
     const [ allPriority, setPriority ] = useState([])
-    //const { currentUser, logout } = useAuth()
-    const [email, setEmail] = useState('')
+    const { currentUser, logout } = useAuth()
 
     let database = Fire.db;
 
+    const getData = async() =>{
+        const Event = []
+        tests.getCollection('Events').get()
+        .then(querySnapshot => {
+            querySnapshot.docs.forEach(doc => {
+                allTitle.push(doc.data())
+            });
+            // setFoodItems(foodItems)
+            // this.setState({show: false});
+        }).catch(function(error){
+            console.log(error)
+        })
+    }
 
-const getData = async() =>{
-    const Event = []
-    tests.getCollection('Events').get()
-    .then(querySnapshot => {
-        querySnapshot.docs.forEach(doc => {
-            allTitle.push(doc.data())
-        });
-        // setFoodItems(foodItems)
-        // this.setState({show: false});
-    }).catch(function(error){
-        console.log(error)
-    })
-}
-
-
-async function addFooditem(){
     
-    let id="d"+foodNameValue.split(" ").join("")
-    await tests.getCollection('Events').doc(id).set(
-        {name: foodNameValue, 
-            price: foodPriceValue, 
-            description: foodDescriptionValue, 
-            id: id, 
-            Chef: username, 
-            url: url, 
-            rating:[], 
-            count:0, 
-            vip:vip, 
-            email:email })
-    .then(() =>{
-        console.log("Added Event to DB")
-    })
-    .catch(function(error) { //broke down somewhere
-        console.error("Error: ", error);
-    });
-        
-    
-    setAllTitle("")
-    setAllDescription("")
-    setAllProgress(false)
-    setAllStart("") //change it to timestamp
-    setAllEnd("") //change it to timestamp
-    setPriority("")
-
-
-    getData()
 }
 
 export default AddEvents;
 
-/* 
-async function deleteEventItem(fooditem){
-    if(fooditem.id[0] == ("d")){
-        await tests.getCollection('Drink').doc(fooditem.id).delete()
-        .then(() =>{
-            console.log("Removed drink item from Database")
-            getData()
+/*
+    const getData = async() =>{
+        const Event = []
+        tests.getCollection('Events').get()
+        .then(querySnapshot => {
+            querySnapshot.docs.forEach(doc => {
+                allTitle.push(doc.data())
+            });
+            // setFoodItems(foodItems)
+            // this.setState({show: false});
+        }).catch(function(error){
+            console.log(error)
+        })
+    }
 
+    const getData = async() =>{
+        await db.getCollection("Events").get().then(snapshot => {
+            const tempcourseName= [];
+            snapshot.forEach(doc => {
+                const data = doc.data();
+                if (currentUser.email === doc.id){
+                    let x =0;
+                    for(x = 0; x < data.ListofCourses.length; x++){
+                        tempcourseName.push(data.ListofCourses[x]);
+                    }
+                    
+                }
+
+            })
+            setCourseName(tempcourseName);
+        }).catch(error => console.log(error))
+        }          
+    
+    useEffect(() =>{
+        getData()
+    },[])
+
+
+    async function addFooditem(){
+        
+        let id="d"+foodNameValue.split(" ").join("")
+        await tests.getCollection('Events').doc(id).set(
+            {   allTitle: alltitleValue,               
+                alldescription: allDescriptionValue, 
+                allStart: allStartValue,
+                allEnd: timestampeValue, 
+                allPriority: allPriorityValue, 
+                email:email })
+        .then(() =>{
+            console.log("Added Event to DB")
         })
         .catch(function(error) { //broke down somewhere
             console.error("Error: ", error);
         });
-
+        setAllTitle("")
+        setAllDescription("")
+        setAllProgress(false)
+        setAllStart("") //change it to timestamp
+        setAllEnd("") //change it to timestamp
+        setPriority("")
+        getData()
     }
-   
-} */
+}
+
+<Modal show={show} onHide={handleClose}>
+    <Modal.Header>
+        <Modal.Title>Food</Modal.Title>
+    </Modal.Header>
+        <Modal.Body>
+            <Form onSubmit={addFooditem}>
+                 <div>
+                      <h6>Item Name:</h6>
+                         <input type="text" className="post-modal-input" onChange={e => setFoodName(e.target.value)} value={foodNameValue}/>
+                      <h6>Price:</h6>
+                         <input type="number" className="post-modal-input" onChange={e => setFoodPrice(e.target.value)} value={foodPriceValue}/>
+                      <h6>Description:</h6>
+                         <input type="text" className="post-modal-input" onChange={e => setFoodDescription(e.target.value)} value={foodDescriptionValue}/>
+                      <h6>Type:</h6>
+                <select value={typeValue} onChange={e => setFoodType(e.target.value)}>
+                    <option value="food">Food</option>
+                    <option value="drink">Drink</option>
+                </select>
+                <h6>Url:</h6>
+                <input type="text" className="post-modal-input" onChange={e => setURL(e.target.value)} value={url}/>
+                <h6>Only for VIP:</h6>
+                <input type="checkbox" style={{marginLeft:"-45%"}} className="post-modal-input" onChange={e => setVIP(e.target.checked)}/>
+
+        </div>
+    </Form>
+</Modal.Body>
+<Modal.Footer>
+<Button variant="secondary post-modal-button" onClick={handleClose}>
+    Close
+</Button>
+<Button variant="primary post-modal-button" onClick={addFooditem}>
+    Save Changes
+</Button>
+</Modal.Footer>
+</Modal>
+*/
--- src/components/Calendar/MyCalendar.js
+++ src/components/Calendar/MyCalendar.js
@@ -2,76 +2,178 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./MyCalendar.css";
+import {useAuth} from '../../context/AuthContext';
+import Fire from '../../firebase';
+import firebase from 'firebase/app';
+import { Link, useHistory } from "react-router-dom"
 
-const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
+export const db = Fire.db;
 
-const events = [
-    {
-        title: "Group meeting",
-        allDay: true,
-        start: new Date(2021, 9, 29),
-        end: new Date(2021 ,9, 29),
-        cName:"Event",
-    },
-    {
-        title: "1st-iteration",
-        allDay: true,
-        start: new Date(2021, 10, 3),
-        end: new Date(2021 ,10, 3),
-        cName:"Event",
-    },
-    {
-        title: "2st-iteration",
-        allDay: true,
-        start: new Date(2021, 10, 17),
-        end: new Date(2021 ,10, 17),
-        cName:"Event",
-    },
-];
 
-function MyCalendar() {
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
+export default function MyCalendar() {
 
-    function handleAddEvent() {
-        setAllEvents([...allEvents, newEvent]);
+    const [ allTitle, setTitle ] = useState([])
+	const [ allDescription, setDescription ] = useState([])
+	const [ allProgress, setProgress ] = useState([])
+    const [ allStart, setStart ] = useState([])
+    const [ allEnd, setEnd ] = useState([])
+    const [ allPriority, setPriority ] = useState([])
+    const [ error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    const history = useHistory();
+    const [loading, setLoading] = useState(false)
+
+    const allTitleRef = useRef();
+    const allDescriptionRef = useRef();
+    const allProgressRef = useRef();
+    const allStartRef = useRef();
+    const allEndRef = useRef();
+    const allPriorityRef = useRef();
+    const EventList = [];
+
+    db.getCollection('Users').doc(currentUser.email).get().then((doc) => {
+        if(doc.exists) {
+            setTitle(doc.data().allTitle);
+            setDescription(doc.data().allDescription);
+            setPriority(doc.data().allPriority);   
+            setStart(doc.data().allStart);  
+            setEnd(doc.data().allEnd);  
+        }
+        else {
+            return;
+        }
+    });
+
+    const getEvents = async () => {
+        await db.getCollection('Tasks').doc(currentUser.email).get().then((d) => {
+            if (d.exists)  setTitle(d.data().allTitle)           
+        })
+    }
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    async function handleLogout() {
+        setError("")
+        try {
+            await logout()
+            history.push("/Login")
+        } catch {
+            setError("Failed to log out")
+        }
+    }
+
+    const getData = async() =>{
+        await db.getCollection("Course").get().then(snapshot => {
+            const tempEventName= [];
+            snapshot.forEach(doc => {
+                const data = doc.data();
+                if (currentUser.email === doc.id){
+                    let x =0;
+                    for(x = 0; x < data.ListofEvents.length; x++){
+                        tempEventName.push(data.ListofEvents[x]);
+                    }
+                    
+                }
+
+            })
+            setTitle(tempEventName);
+        }).catch(error => console.log(error))
+        }
+        
+    function pushContent(){
+        EventList.push(allTitleRef)
+        EventList.push(allDescriptionRef)
+        EventList.push(allProgressRef)
+        EventList.push(allStartRef)
+        EventList.push(allEndRef)
+        EventList.push(allPriorityRef)
+    }
+    
+    useEffect(() =>{
+        getData()
+    },[])
+
+    /* bla bla*/
+
+    function printTable(){
+        try{
+        let eventslength = EventList.length;
+            
+        const list = []
+        let x;
+        for(x = 0; x < eventslength; x++){
+            list.push(<li>{EventList[x].Title} - {EventList[x].Description} </li>)
+        }
+
+        return(
+            <div>
+                {list}
+            </div>
+        )
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        try {
+            setError('')
+            setLoading(true)
+
+            db.getCollection('Course').doc(currentUser.email).update({
+                ListofCourses: firebase.firestore.FieldValue.arrayUnion(
+                    {   
+                        Description: allDescriptionRef.current.value,
+                        End: allEndRef.current.value,
+                        Priority: allPriorityRef.current.value,
+                        Progress: allProgressRef.current.value,
+                        Start: allStartRef.current.value,
+                        Title: allTitleRef.current.value
+                        }
+                    )
+                }).then(function() {// went through
+                    console.log("Document successfully written!");
+                    refreshPage()
+                })
+                .catch(function(error) { //broke down somewhere
+                    console.error("Error writing document: ", error);
+                });
+            history.push('/Profile')
+        } catch{
+            setError('Failed to add event')
+        }
+        setLoading(false)
     }
 
     return (
             <div className="App font-style-Alice">
-                <div className="Container">
-                    <div className="Cal_Header">
-                        <h1 className="1_header">Calendar</h1>
-                    </div>
-                    <div className = "info">
-                        <h2 className="2_header">Add New Event</h2>
-                        <div className="menu">
+                        <tbody>
+                            <tr><td><a href = '/Courses'>{printTable()}</a></td></tr>
+                        </tbody>
+            </div>
+    );
+}
+
+/*
                             <input className="DataPicker_1" type="text" placeholder="Add Title" value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />                                <DatePicker className="DataPicker_1" placeholderText="Start Date" selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
                             <DatePicker className="DataPicker_1" placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
                             <button className="Button" onClick={handleAddEvent}>
                                 Add Event
                             </button>
-                        </div>
-                    </div>
-                    <Calendar className="Calen" localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
-                </div>
-            </div>
-    );
-}
 
-export default MyCalendar;
+                            <Calendar className="Calen" localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+                
+*/
