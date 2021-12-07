38ac4344965faeacde24b8e39df217ceee3d58d9
mitchellmui17 <mitchellmui17@gmail.com>
2021-12-07 17:56:31 -0500

    UT For Navbar not necessary
    Removed unneeded calls for Navbar and deleted navbar test
    + fixed up UT for Signup

--- src/components/NavBar/__test__/NavBar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './../NavBar';
-
-it('renders without crashing', () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Navbar></Navbar>, div);
-});
